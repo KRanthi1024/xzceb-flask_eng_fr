09e32e58fd4b97975ef199a3bf072b222f299c75
https://github.com/KRanthi1024/xzceb-flask_eng_fr <Kranthi1024@gmail.com>
2023-06-06 08:30:27 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/translator.py
@@ -0,0 +1,17 @@
+from deep_translator import MyMemoryTranslator
+'''
+This provide the necessary credentials and set up the enviroment
+'''
+
+
+#Translation from english to french
+def english_to_french(english_text):
+    """Function that translate english to french"""
+    french_text= MyMemoryTranslator(source='english', target='french').translate(english_text)
+    return french_text
+
+#Translation from french to english
+def french_to_english(french_text):
+    """Function that translate french to english"""
+    english_text= MyMemoryTranslator(source='french', target='english').translate(french_text)
+    return english_text
